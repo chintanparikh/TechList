fe5bd9d9bd0a79d50cd2e696d12dbea813c439fd
Chintan Parikh <chintan@thefrontiergroup.com.au>
2012-08-03 15:53:08 +0800

    User + user spec

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,9 +1,11 @@
 class User < ActiveRecord::Base
   belongs_to :role
 
-  Role.enumeration do |role_name|
-    define_method "#{role_name}?" do
-      self.role_name.to_s == role_name.to_s
+  before_save :default_user_role
+
+  Role.enumeration.each do |role_name|
+    define_method "#{role_name.to_s}?" do
+      self.role.name.to_s == role_name.to_s
     end
   end
   # Include default devise modules. Others available are:
@@ -15,4 +17,12 @@ class User < ActiveRecord::Base
   # Setup accessible (or protected) attributes for your model
   attr_accessible :email, :password, :password_confirmation, :remember_me
   # attr_accessible :title, :body
+
+protected
+
+  def default_user_role
+    if role.nil?
+      self.role = Role.find_by_name('user')
+    end
+  end
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,26 @@
 require 'spec_helper'
 
 describe User do
-  pending "add some examples to (or delete) #{__FILE__}"
+  let(:admin) { FactoryGirl.create(:admin) }
+  let(:user) { FactoryGirl.create(:user) }
+
+  describe ".admin?" do
+    it "should return true for an admin" do
+      admin.admin?.should be_true
+    end
+
+    it "should return false for a user" do
+      user.admin?.should be_false
+    end
+  end
+
+  describe ".user?" do
+    it "should return false for an admin" do
+      user.admin?.should be_false
+    end
+    it "should return true for a user" do
+      user.user?.should be_true
+    end
+  end
 end
+
