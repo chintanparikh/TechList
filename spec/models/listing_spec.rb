be5999626396f02feaf2a9a33822bdcef9ab93e0
Chintan Parikh <parikhcv@gmail.com>
2012-08-09 11:16:10 +0800

    Add specs for assocation

--- spec/models/listing_spec.rb
+++ spec/models/listing_spec.rb
@@ -3,5 +3,6 @@ require 'spec_helper'
 describe Listing do
   it { should allow_mass_assignment_of(:content) }
   it { should allow_mass_assignment_of(:name) }
-  it { should belong_to(:section)}
+  it { should belong_to(:section) }
+  it { should belong_to(:user) }
 end
